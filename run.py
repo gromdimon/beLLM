c04e061deaaf8b481f2fbdfe99a7f940950d9374
gromdimon <grom.dima.grom@gmail.com>
2024-01-26 16:22:16 +0100

    add generation mode and wirte valid Readme

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 This file is used to run the model.
 """
 
+import argparse
 import logging
 import time
 
@@ -13,7 +14,16 @@ from train import config
 # Setting up logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Generate text using the Bigram Language Model.")
+    parser.add_argument("--prompt", type=str, default="", help="Initial text to start generation")
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
+    args = parse_arguments()
+
     start_time = time.time()
     logging.info("Loading model...")
     logging.info("Config: %s", config)
@@ -37,7 +47,13 @@ if __name__ == "__main__":
 
     # generate
     start_generation_time = time.time()
-    context = torch.zeros((1, 1), dtype=torch.long)
+    initial_context = args.prompt if args.prompt else " "
+    if len(initial_context) > 256:
+        raise ValueError("Initial context must be less than 256 characters.")
+    context = torch.tensor([encode(initial_context)], dtype=torch.long)
     generated_text = decode(model.generate(context, max_new_tokens=2000)[0].tolist())
+    # save the generated text in generations/ folder
+    with open(f"generations/{generated_text[:6]}.txt", "w") as f:
+        f.write(generated_text)
     logging.info(generated_text)
     logging.info("Generation completed in {:.2f}s".format(time.time() - start_generation_time))
